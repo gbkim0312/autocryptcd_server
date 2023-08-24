b9de79030a330872a12585dea6bea1b4bb30f0c8
“gibeom_kim” <gbkim@autocrypt.io>
2023-08-24 01:36:35 +0000

    modifty args

--- app.js
+++ app.js
@@ -1,60 +1,68 @@
+// app.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const { spawn } = require('child_process');
 
 const app = express();
-app.use(bodyParser.json()); //json 형태의 Body를 Parsing함
+app.use(bodyParser.json());
 
+// POST 요청이 '/run-script' 경로로 들어올 때
 app.post('/run-script', (req, res) => {
-  // body를 Option으로 불러옴
-  const option = req.body;
+  const option = req.body; // 클라이언트에서 온 요청의 바디에 있는 옵션 정보를 받음
 
-  // 클라이언트가 올바른 요청을 보냈는지 확인
-  if (!option) {
-    return res.status(400).json({ error: 'Invalid or missing option in the request body.' });
+  if (!option) { // 옵션이 없으면 400 에러 응답
+    return res.status(400).json({ error: '요청 바디에 유효하지 않거나 빠진 옵션이 있습니다.' });
   }
 
-  // 받은 옵션 출력
-  console.log('Received Options:', option);
+  console.log('Received Options:', option); // 받은 옵션 출력
 
-  // 'command' 필드가 'build'일 경우에만 처리
-  if (option.command === 'build') {
+  if (option.command === 'build') { // 옵션의 'command'필드의 값이 'build'일 경우
     console.log('빌드 스크립트 실행 중');
-
-    // 서버가 클라이언트로 실시간 데이터 전송 설정
+    // 응답 헤더 설정
     res.setHeader('Content-Type', 'text/event-stream');
     res.setHeader('Cache-Control', 'no-cache');
     res.setHeader('Connection', 'keep-alive');
 
-    // 빌드 스크립트에 전달할 인자들 설정
+    // client로 부터 받은 option을 args로 만들어서 빌드 스크립트로 전달
     const args = [
-      option.timestamp,                // 타임스탬프
-      option.type,                     // 타입 (device or Commit Number)
-      option.toolchain,                // 툴체인
-      option.standard,                 // 표준 (camp122, ydt3957, esti...)
-      option.rel || "",                // 릴리즈 버전 (값이 없을 경우 빈 문자열로 설정)
-      option.hw || "",                 // 하드웨어 (값이 없을 경우 빈 문자열로 설정)
-      option.deviceName || ""          // 디바이스 이름 (값이 없을 경우 빈 문자열로 설정)
+      option.timestamp,
+      option.type,
+      option.toolchain,
+      option.standard,
+      option.deviceName,
     ];
 
-    // 빌드 스크립트 실행 및 로그 처리 (자식 프로세스에서 진행)
+    // 값이 null이 아닐 때만 args에 추가
+    if (option.rel !== null && option.rel !== undefined) {
+      args.push(option.rel);
+    } else {
+      args.push(""); // 값이 null일 경우 빈 문자열을 추가하여 순서를 유지
+    }
+
+    if (option.hw !== null && option.hw !== undefined) {
+      args.push(option.hw);
+    } else {
+      args.push("");
+    }
+
+    // 받은 옵션을 가지고 빌드 스크립트 실행
     const buildProcess = spawn('/workdir/securityplatform/scripts/test_build.sh', args, { cwd: '/workdir/securityplatform' });
 
-    // 빌드 스크립트의 출력을 클라이언트로 전송
+    // 표준 출력 로그를 클라이언트로 전송
     buildProcess.stdout.on('data', (data) => {
       const logMessage = data.toString();
       console.log(logMessage);
       res.write(`${logMessage}`);
     });
 
-    // 빌드 스크립트의 오류 출력을 클라이언트로 전송
+    // 에러 로그를 클라이언트로 전송
     buildProcess.stderr.on('data', (data) => {
       const errorMessage = data.toString();
       console.error(errorMessage);
       res.write(`${errorMessage}\n`);
     });
 
-    // 스크립트 실행 완료 후 처리
+    // 스크립트 실행 종료 처리
     buildProcess.on('close', (code) => {
       if (code === 0) {
         console.log('빌드 스크립트 실행 성공');
@@ -66,12 +74,11 @@ app.post('/run-script', (req, res) => {
       res.end();
     });
   } else {
-    // 'command' 필드가 'build'가 아닐 경우 에러 반환
-    return res.status(400).json({ error: 'Invalid script name in buildOptions.' });
+    return res.status(400).json({ error: '' });
   }
 });
 
-// 앱 포트 설정
+// 서버 포트 설정후 Listen
 const port = 10101;
 app.listen(port, () => {
   console.log(`App listening on port ${port}.`);
